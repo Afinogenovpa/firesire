058fbd3f581e4b20df269353e7543ac40e626e62
pashket <afinogenov.p@gmail.com>
2020-10-03 16:40:33 +0300

    add index

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 firesire = Flask(__name__)
 
+
 @firesire.route('/')
 def index():
-    return 'index page'
+    return render_template('index.html')
 
-
+#@firesire.route('/<username>')
+#def users():
+#    return 'User %s' % username
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    firesire.debug = True
     firesire.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
